91aa973dc90f806f720074a1d5cb45dfc4f88a0e
papingothegamer <toluwanimidaramola8@gmail.com>
2023-07-26 17:40:00 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+import {
+    IoCodeWorking,
+    IoLogoGithub,
+    IoLogoTwitter,
+    IoLogoLinkedin,
+    IoLogoWhatsapp,
+  } from "react-icons/io5";
+  import img1 from "./img/ecommerce.png"; 
+  
+  
+  
+  export const Experience = [
+    {
+      id: 1,
+      date: "April 2023 - present",
+      iconsSrc: <IoCodeWorking />,
+      title: "Junior Frontend Developer",
+      location: "Integrify Finland",
+      description:
+        "Full Stack Development",
+    },
+    {
+      id: 2,
+      date: "January 2020 - present",
+      iconsSrc: <IoCodeWorking />,
+      title: "Student",
+      location: "Ternopil National Technical University, Ternopil, Ukraine",
+      description:
+        "B.Eng Computer Engineering",
+    },
+    {
+      id: 3,
+      date: "2018-2020",
+      iconsSrc: <IoCodeWorking />,
+      title: "Student",
+      location: "Elizade University, Nigeria",
+      description:
+        "B.Eng Computer Engineering",
+    },
+];
+  
+  export const Projects = [
+   
+  {
+    id: 1,
+    name: "eCommerce App ",
+    imageSrc: img1,
+    techs: "React JS, Material UI, Redux",
+    github: "#",
+  },
+
+  ];
+  
+  export const SocialLinks = [
+    {
+      id: 1,
+      iconSrc: <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />,
+      name: "GitHub",
+      link: "#",
+    },
+    {
+      id: 1,
+      iconSrc: (
+        <IoLogoTwitter className="text-blue-500 text-3xl cursor-pointer" />
+      ),
+      name: "Twitter",
+      link: "#",
+    },
+    {
+      id: 1,
+      iconSrc: (
+        <IoLogoLinkedin className="text-blue-800 text-3xl cursor-pointer" />
+      ),
+      name: "LinkedIn",
+      link: "#",
+    },
+    {
+      id: 1,
+      iconSrc: (
+        <IoLogoWhatsapp className="text-green-500 text-3xl cursor-pointer" />
+      ),
+      name: "Whatsapp",
+      link: "#",
+    },
+  ];
